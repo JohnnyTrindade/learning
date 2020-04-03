8616915071094c0f779519773a18c92f330755e1
JohnnyTrindade <johnnywelton@live.com>
2020-04-03 09:30:41 -0300

    PHP basics last part

---
+++ php/php-html.php
@@ -0,0 +1,16 @@
+
+<?php
+$nome = "Johnny Welton";
+$html = "<h1>Iniciando com PHP e HTML</h1>";
+?>
+<html>
+<head></head>
+<body>
+<?php
+echo 5 + 3;
+echo $nome;
+?>
+<?php echo $nome; ?>
+<?php echo $html; ?>
+</body>
+</html>
