6f178586c353cd9f83583771c13697654a7db15b
JohnnyTrindade <johnnywelton@live.com>
2020-04-09 09:21:07 -0300

    Avançando com PHP - 2

---
+++ avancando-php/condicional-final.php
@@ -0,0 +1,25 @@
+<?php
+
+$media = 7;
+$media_recuperacao = 5;
+$frequencia = 70;
+
+$media_aluno = 5;
+$frequencia_aluno = 40;
+
+$reprovado_por_faltas = $frequencia_aluno < $frequencia ? true:false;
+
+
+if (!$reprovado_por_faltas) {
+     if ($media_aluno < $media_recuperacao) {
+         echo "Reprovado!";
+     }
+     elseif ($media_aluno < $media) {
+         echo "Recuperação";
+     }
+     else {
+         echo "Aprovado!";
+     }
+} else {
+    echo "Reprovado por faltas!";
+}
