42855e79a8422a14773554575a42c1932da39b17
JohnnyTrindade <johnnywelton@live.com>
2020-04-16 21:49:28 -0300

    Funções PHP

---
+++ avancando-php/funcoes.php
@@ -0,0 +1,13 @@
+<?php
+
+function escreva_nome($saudacao, $nome) {
+    echo "$saudacao, $nome! Como vai?<br>";
+}
+
+escreva_nome("Bom dia", "Johnny");
+
+escreva_nome("Boa tarde","Gilson");
+
+escreva_nome("Boa noite","Marco");
+
+//Funções facilitam o reuso do código e simplificam seu trabalho
---
+++ avancando-php/funcoes_retorno.php
@@ -0,0 +1,10 @@
+<?php
+
+function soma($a, $b) {
+    return $a + $b . "<br>";
+}
+
+$resultado = soma(2, 3);
+
+echo $resultado * 3;
+
