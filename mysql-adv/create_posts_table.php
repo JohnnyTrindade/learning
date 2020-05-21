26e9f6b78e7a4846d15310e1cd8466ebd25d559a
JohnnyTrindade <johnnywelton@live.com>
2020-05-21 16:49:28 -0300

    Avançando php complementos

--- mysql-adv/create_posts_table.php
+++ mysql-adv/create_posts_table.php
@@ -1,15 +1,19 @@
 <?php
 
-require __DIR__.'/utils/connection.php';
+$conn = require __DIR__.'/utils/connection.php';
 
+$conn->query('DROP TABLE posts');
 
-$sql = 'CREATE TABLE posts(
-            id INT AUTO_INCREMENT PRIMARY KEY,
-            title VARCHAR(50) NOT NULL,
-            body TEXT NOT NULL
-)';
+$sql = '
+    CREATE TABLE posts(
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        title VARCHAR(50) NOT NULL,
+        body TEXT NOT NULL,
+        FULLTEXT KEY title (title, body)
+    )
+';
 
-if ($conn->query($sql)) {
+if (!$conn->query($sql)) {
     die('Error: table exists');
 }
 
--- mysql-adv/utils/connection.php
+++ mysql-adv/utils/connection.php
@@ -1,10 +1,24 @@
 <?php
 
+$debug = true;
+
+if ($debug) {
+    /**
+     * MYSQLI_REPORT_ERROR
+     * MYSQLI_REPORT_OFF
+     * MYSQLI_REPORT_STRICT
+     * MYSQLI_REPORT_INDEX
+     * MYSQLI_REPORT_ALL
+     */
+    mysqli_report(MYSQLI_REPORT_ERROR);
+} else {
+    mysqli_report(MYSQLI_REPORT_OFF);
+}
+
 $conn = new mysqli('localhost','root','1q2w3e4r', 'test');
 
 if ($conn->connect_errno) {
-    die('Falhou em conectar' . $conn->connect_errno);
-
+    die('Falhou em conectar: ' . $conn->connect_errno);
 }
 
 return $conn;
